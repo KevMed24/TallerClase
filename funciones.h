1feb5bb79cf0da0235754f2c7089511f6c76d5fb
KevMed24 <kevin.medina.pantoja@udla.edu.ec>
2024-05-10 13:29:53 -0500

    inicio

---
+++ funciones.h
@@ -0,0 +1,11 @@
+//Prototipado
+float calcular_interes_mensual(float i);
+float calcular_mes_1(float monto, float tasa_anual);
+float calcular_mes_2(float monto, float tasa_anual);
+float calcular_mes_3(float monto, float tasa_anual);
+float calcular_mes_4(float monto, float tasa_anual);
+float calcular_mes_5(float monto, float tasa_anual);
+float calcular_mes_6(float monto, float tasa_anual);
+float calcular_ahorro_seis_meses(float monto);
+float calcular_total(float ahorro_seis_meses, float interes_ganado); 
+void imprimir_tabla_resultados(float Itm1, float Itm2, float Itm3, float Itm4, float Itm5, float Itm6);
